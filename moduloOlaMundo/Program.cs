a3636006f4a6e5bf40e91595534bf57a296fea5e
Joyce Pereira Oliveira <99741555+JoycePereiraOliveira@users.noreply.github.com>
2022-10-03 19:07:38 -0300

    desafio01

---
+++ moduloOlaMundo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moduloOlaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercício - "Olá Mundo!"
+
+            // Primeira linha de código
+
+            // Console.WriteLine("Hello World!");
+
+            //  Console.Write("Congratulations!");
+            //  Console.Write(" ");
+            //  Console.Write("You wrote your first lines of code!");
+
+            // O compilador converte o código-fonte em um formato diferente que a CPU...
+            // ... do computador pode executar;
+            // Aspas duplas em frases: chamada de cadeia de caracteres literal;
+            // Após um método, sempre há um conjunto de parênteses;
+            // Os parênteses são conhecidos como o operador de invocação de método;
+            // As classes são "proprietárias" de métodos. Os métodos residem em uma classe;
+            // O ponto(.) entre a classe(Console) e o método(WriteLine), é o operador de acesso a membro...  
+            // ... A forma como você navega da classe para um dos métodos dela;
+            // O ponto e vírgula(;) é o final do operador de instrução. Uma instrução é uma instrução completa em C#...
+            // ... O ponto e vírgula informa ao compilador que terminamos de inserir o comando;
+
+
+            // Desafio 01 - Escrever o código no Editor do .NET para exibir duas mensagens. Usando duas técnicas diferentes:
+
+            Console.WriteLine("This is the first line.");
+
+                Console.Write("This the");
+                Console.Write(" ");
+                Console.Write("second line.");
+
+        }
+    }
+}
